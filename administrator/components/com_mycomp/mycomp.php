921882802ae54b95f35bde98af66848e049e4650
Aphikrit Saravasri <shinya_kazuma@hotmail.com>
2015-06-17 11:51:54 +0700

    Add menu option

---
+++ administrator/components/com_mycomp/mycomp.php
@@ -0,0 +1,4 @@
+<?php
+defined('_JEXEC') or die;
+
+echo "<h2>Welcome to my Administrator Component</h2>";
---
+++ components/com_mycomp/mycomp.php
@@ -0,0 +1,4 @@
+<?php
+defined('_JEXEC') or die;
+
+echo "<h2>Welcome to my Front_End Component</h2>";
