8a80db8985b3c948349fdc753e3c6e8988d9447a
Aphikrit Saravasri <shinya_kazuma@hotmail.com>
2015-06-18 11:07:32 +0700

    Add CSS and JS

--- administrator/components/com_mycomp/controller.php
+++ administrator/components/com_mycomp/controller.php
@@ -5,13 +5,28 @@ defined('_JEXEC') or die();
 */
 class MyCompController extends JControllerAdmin
 {
-	
 	function display($cachable = false, $urlparams = false){
+		JToolBarHelper::Title('My Component');
 		echo JText::_('COM_MYCOMP_WELCOME');
 	}
 
 	function create(){
-		echo JText::_('COM_MYCOMP_TASK_CREATE');
+		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/backend_create_task.css');
+		$doc->addScript(JURI::root().'media/com_mycomp/js/backend_createtask.js');
+		JToolBarHelper::Title('Create New Task');
+		echo '<div id="welcome">'.JText::_('COM_MYCOMP_TASK_CREATE').'</div>';
+	}
+
+	function listtask(){
+		JToolBarHelper::Title('List All Tasks');
+		echo JText::_('COM_MYCOMP_TASK_LIST');
+	}
+
+	function help(){
+		JToolBarHelper::Title('Help Documentation');
+		echo JText::_('COM_MYCOMP_TASK_HELP');
+		echo '<h1>This is Help Task <input type="text">';
 	}
 
 	function delete(){
--- components/com_mycomp/controller.php
+++ components/com_mycomp/controller.php
@@ -4,17 +4,20 @@ defined('_JEXEC') or die;
 jimport('joomla.controller');
 class MyCompController extends JControllerAdmin{
 	function display($cachable = false, $urlparams = false){
-		echo "I am Here without task";
+		echo 'I am Here without task';
 	}
 
 	function create(){
-		echo "Welcom to Create the task";
+		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/frontend_create_task.css');
+		$doc->addScript(JURI::root().'media/com_mycomp/js/frontend_createtask.js');
+		echo '<div id="welcome">Welcome to Create the task</div>';
 	}
 
 	function delete(){
 		$app=JFactory::getApplication();
 		$id=JRequest::getVar('id');
-		echo "You want to delete $id";
+		echo 'You want to delete $id';
 		$app->close();
 	}
 }
